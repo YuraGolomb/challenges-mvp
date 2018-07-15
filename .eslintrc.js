9dc72b5934b0d4a8ea391e36ac8e90a6ebcfb4e4
YuraGolomb <yuragolomb@gmail.com>
2018-07-15 20:16:23 +0300

    init react with next

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "extends": "airbnb",
+  "rules": {
+    "react/react-in-jsx-scope": 0,
+    "react/jsx-filename-extension": 0,
+    "react/jsx-one-expression-per-line": 0,
+  }
+};
